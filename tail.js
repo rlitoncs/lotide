02d9c8cd299e95a23163a71cc9ecdd5948070280
rlitoncs <rj_liton@hotmail.com>
2024-04-18 00:54:50 -0400

    Added tail function

---
+++ tail.js
@@ -0,0 +1,40 @@
+// FUNCTION IMPLEMENTATION
+/**
+ *
+ * assertEqual function expects two arguments and returns a passed assertion or a failed assertion, depending on whether the two arguments strictly match
+ *
+ * assertEqual uses a ternary operator to decide whether the two arguments are strictly equal and outputs the assertion message
+ *
+ * Reminder: eslint assertEqual.js
+ */
+
+const assertEqual = function(actual, expected) {
+  // If actual and expected values are equal, print success message. Otherwise, print failure message.
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const tail = function (array){
+  return array.slice(1);
+}
+
+
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: Array with one element
+const result2 = tail([1]);
+assertEqual(result2[0], undefined);
+
+// Test Case 3: Empty Array
+const result3 = tail([]);
+assertTequal(result3[0], undefined);
