d07b0094d9bc4f7b2ea5baebdb55f1294c28caeb
rlitoncs <rj_liton@hotmail.com>
2024-04-17 23:42:25 -0400

    Added a complex test case

--- head.js
+++ head.js
@@ -25,4 +25,4 @@ assertEqual(head([5, 6, 7]), 5); //testing with numbers
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //testing with strings
 assertEqual(head(["OneElement"]), "OneElement"); //testing with 1 element
 assertEqual(head([]), undefined); //testing with no elements
-//unsure how this would assert assertEqual(head([[]]), "object"); 
+assertEqual(Array.isArray(head([[]])), true); //testing if first element is an array itself
