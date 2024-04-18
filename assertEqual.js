e58e1fd90eafc8b2dca4ced95d62bf38d1542f89
rlitoncs <rj_liton@hotmail.com>
2024-04-17 22:35:17 -0400

    Implemented Function to assert values

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+/**
+ *
+ * assertEqual function expects two arguments and returns a passed assertion or a failed assertion, depending on whether the two arguments strictly match
+ *
+ * assertEqual uses a ternary operator to decide whether the two arguments are strictly equal and outputs the assertion message
+ *
+ * Reminder: eslint assertEqual.js
+ */
+
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
