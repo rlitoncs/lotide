f99d89f27e0a57013f70ad4edc0789d3cd38fc72
rlitoncs <rj_liton@hotmail.com>
2024-04-19 22:46:22 -0400

    Refactor middle function again

--- middle.js
+++ middle.js
@@ -34,11 +34,6 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 // MIDDLE Function Implementation
-// toSpliced() is a method that only works on newer version of node. toSpliced() does not alter the original array since it makes a new copy of it.
-// splice() method alters the original contents of the array
-
-// to go around this we use the .slice() method since it returns a copy of an array
-
 const middle = function(sourceArr) {
   const sourceArrCopy = sourceArr.slice(); //returns a copy of sourceArr so original contents are not modified
 
@@ -55,7 +50,6 @@ const middle = function(sourceArr) {
   // Case 3: More than 2 elements
   // a) remove 1 element if sourceArr length is odd
   if (sourceArrCopy.length % 2 === 1) {
-    console.log([sourceArrCopy[(sourceArrCopy.length - 1) / 2]]);
     return [sourceArrCopy[(sourceArrCopy.length - 1) / 2]]; // wrap the result in another array so it returns an array with the middle elem
   }
   
