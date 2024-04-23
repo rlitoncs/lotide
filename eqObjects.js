d22513cbbd9069fb59d5d62462b3e1d21295ab10
rlitoncs <rj_liton@hotmail.com>
2024-04-23 17:00:18 -0400

    Refactor eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -3,6 +3,22 @@ const assertEqual = function(actual, expected) {
   console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+// EQUAL ARRAYS Function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let num = 0; num < arr1.length; num++) {
+    if (arr1[num] !== arr2[num]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
 // EQUAL OBJECTS Implementation
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
@@ -11,15 +27,20 @@ const eqObjects = function(object1, object2) {
     return false;
   }
 
-  for (let key in object1) {
-    if (object2[key]) {
-      if (object1[key] !== object2[key]) {
-        return false;
-      }
-    } else {
+  //Checks if current key-value pair are arrays in both objects
+  // If it is, call eqArrays
+  // If not, check if current key-value pair have the same key and value for both objects
+  // If not, return false
+  // Return true if all the key-value pairs have identical keys and value after the loop
+  
+  for (let keys of Object.keys(object1)) {
+    if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
+      return eqArrays(object1[keys], object2[keys]);
+    } else if (object1[keys] !== object2[keys]) {
       return false;
     }
-  } return true;
+  }
+  return true;
 
 };
 
@@ -30,6 +51,17 @@ const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "lon
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
 
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
+
+
 
 /*
 two objects are equal when:
@@ -38,3 +70,17 @@ two objects are equal when:
     The value for each key in one object is the same as the value for that same key in the other object
 
  */
+
+/*
+Possible Solution 2: Primitive as Values
+  // for (let key in object1) {
+  //   if (object2[key]) {
+  //     if (object1[key] !== object2[key]) {
+  //       return false;
+  //     }
+  //   } else {
+  //     return false;
+  //   }
+  // } return true;
+
+*/
