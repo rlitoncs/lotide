c8423d5eab37252eb80634a7bea7101ca76e8ba3
rlitoncs <rj_liton@hotmail.com>
2024-04-24 01:05:13 -0400

    Remove comments

--- findKey.js
+++ findKey.js
@@ -21,9 +21,6 @@ const findKey = function(object, callbackF) {
     }
   }
 
-  // Throws an undefined error if you use object.key instead object[key]. 
-  // I believe it is because object.key is actually looking for a key named "key", and therefore throws undefined. Since we are assigning key as our variable to store each key from the object array, I guess object.key doesn't recognize it as the actual array key.
-
 };
 
 const result = findKey(
@@ -39,18 +36,3 @@ const result = findKey(
 ); // => "noma"
 
 assertEqual(result, "noma"); //PASS
-
-
-
-//Comments
-/**
- *  // for (let key in object) {
-  //   if (callbackF(object[key])) {
-  //     return key;
-  //   }
-  // }
-
-  // In your findKey function, you used a for...in loop to iterate over the object. While this works fine, it's generally recommended to use Object.keys() to get an array of the object's keys and then iterate over that array. This is because for...in will also iterate over an object's prototype properties, which can sometimes lead to unexpected results.
-
-  Don't understand what object's prototype properties mean
- */
