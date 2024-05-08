5492faca8b1b8bf29316c7e225d74c830d2b4c33
rlitoncs <rj_liton@hotmail.com>
2024-05-07 21:08:54 -0400

    Refactor assertArraysEqual function and add assertArraysEqualTest Test Cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,34 +4,17 @@
  * eqArrays() function expects two arguments and returns true upon the 2 arrays matching, or returns false if invalid
  *
  * assertArraysEqual() takes 2 arrays and makes use of the eqArrays() function to check whether the 2 arrays are matching.
+ *
  * Uses a ternary operator to decide whether 2 arrays returned are the same (true) outputs the assertion message
  *
- * Reminder: eslint assertEqual.js
  */
 
-// EQUAL ARRAYS Function
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let num = 0; num < arr1.length; num++) {
-    if (arr1[num] !== arr2[num]) {
-      return false;
-    }
-  }
-
-  return true;
-
-};
+//Require eqArrays module
+const eqArrays = require("./eqArrays");
 
 //ASSERT EQUAL ARRAYS
 const assertArraysEqual = function(arr1, arr2) {
   console.log(eqArrays(arr1,arr2) ? `✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]` : `🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([], []);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+//Require Modules
+const assertArraysEqual = require("../assertArraysEqual");
+
+
+//TEST 
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([], []);
