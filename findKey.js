d3166e18710196cd4cedabfefe6963ed3d969e57
rlitoncs <rj_liton@hotmail.com>
2024-05-07 23:20:41 -0400

    Refactor findKey and add TEST CASES in findKeyTest.js file

--- findKey.js
+++ findKey.js
@@ -2,13 +2,6 @@
  * findKey() function takes in an object and callback Function and returns a key
  */
 
-
-const assertEqual = function(actual, expected) {
-  // If actual and expected values are equal, print success message. Otherwise, print failure message.
-  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
 const findKey = function(object, callbackF) {
   const keys = Object.keys(object);
 
@@ -22,19 +15,5 @@ const findKey = function(object, callbackF) {
   }
 
 };
-
-const result = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-); // => "noma"
-
-assertEqual(result, "noma"); //PASS
 
 module.exports = findKey;
