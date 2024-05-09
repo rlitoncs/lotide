2ea53d3e12bdffdb3abf387c0341b99db993bc5d
rlitoncs <rj_liton@hotmail.com>
2024-05-08 22:42:12 -0400

    Add M&C automated testing for findKeyByValueTest

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,14 +1,32 @@
 //Require Modules
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert
+const { expect } = require("chai");
 const findKeyByValue = require("../findKeyByValue");
 
-
-//TEST CASES
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+//TEST CASES
+describe("#findKeyByValue", () => {
+
+  //Using a value that exists in object
+  it("should return the key 'drama' from bestTVShowsByGenre object", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama")
+  })
+
+  //Using value that does not exist in object
+  it("should return undefined from bestTVShowsByGenre object", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined)
+  })
+
+  //Passing an object that does not exist 
+  it("should return undefined since object does not exist", () => {
+    
+    const badFn = () => findKeyByValue(myObj, "The Wire") ;
+    expect(badFn).to.throw(ReferenceError);;
+
+  })
+})
