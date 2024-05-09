5b7b7ddf1ee7d6483f9901cce38b4046f69e44e0
rlitoncs <rj_liton@hotmail.com>
2024-05-08 23:09:03 -0400

    Add M&C automated testing for eqArraysTest.js file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,39 @@
 //Require Modules
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const eqArrays = require("../eqArrays");
 
 // TEST CODE
 /*
 It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical. We will save this "deeper" problem for another day. */
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3","4"]), false); // => should FAIL
-assertEqual(eqArrays([], []), true); // should PASS
+describe("#eqArrays", () => {
+
+  //comparing identical arrays
+  it("returns true when comparing arrays [1,2,3] and [1,2,3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  //comparing different arrays
+  it("returns false when comparing arrays [1,2,3] and [3,2,1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  //comparing identical arrays
+  it("returns true when comparing arrays ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  //comparing different arrays
+  it("returns false when comparing arrays ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  //comparing different arrays
+  it("returns false when comparing arrays ['1', '2', '3',] and ['1', '2', '3', '4']'", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+  });
+
+  //comparing identical arrays
+  it("returns true when comparing arrays [] and []'", () => {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+});
