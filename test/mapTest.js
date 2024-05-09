3a8288824306e684dca9e71bc715ab3a86b1daa3
rlitoncs <rj_liton@hotmail.com>
2024-05-09 00:18:01 -0400

    Add M&C automated testing to mapTest.js

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,18 +1,27 @@
 //Require Modules
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const map = require("../map");
-
-//TEST CASES
 
 const words = ["ground", "control", "to", "major", "tom"];
 const arrayOfArrays = [['hello', 'there'], ['Second', 'Array'], ['Interesting', 'bear']];
 const numbers = [5,10,15];
 
-const results1 = map(words, (word) => word[0]);
-const results2 = map(arrayOfArrays, (innerArray) => innerArray[0]);
-const results3 = map(numbers, (number) => number*2);
+//TEST CASES
+describe("#map", () => {
 
+  //Words
+  it("given words ['ground', 'control', 'to', 'major', 'tom'] should return ['g', 'c', 't', 'm', 't']", () => {
+    assert.deepEqual(map(words, (word) => word[0]),['g', 'c', 't', 'm', 't']);
+  });
 
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); //PASS
-assertArraysEqual(results2, ['hello','Second', 'Interesting']); //PASS
-assertArraysEqual(results3, [10,20,30]);// PASS
+  //Array of Arrays
+  it("given arrayOfArrays [['hello', 'there'], ['Second', 'Array'], ['Interesting', 'bear']] should return  ['hello','Second', 'Interesting']", () => {
+    assert.deepEqual(map(arrayOfArrays, (innerArray) => innerArray[0]), ['hello','Second', 'Interesting']);
+  });
+  
+  //Numbers
+  it("given numbers [5,10,15] should return [10,20,30]", () => {
+    assert.deepEqual(map(numbers, (number) => number * 2), [10,20,30]);
+  });
+
+});
