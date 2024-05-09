a3b7d1796d08975d0f88fd78fb095fc1e0cd5526
rlitoncs <rj_liton@hotmail.com>
2024-05-09 00:09:36 -0400

    Add M&C automated testing for findKeyTest.js

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,8 +1,7 @@
 //Require Modules
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const findKey = require("../findKey");
 
-//TEST CASES
 const result = findKey(
   {
     "Blue Hill": { stars: 1 },
@@ -15,4 +14,19 @@ const result = findKey(
   (x) => x.stars === 2
 ); // => "noma"
 
-assertEqual(result, "noma"); //PASS
+console.log(`result object:\n  {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+}\nCallback Function: (x) => x.stars === 2 `)
+
+
+//TEST CASES
+describe("#findKeyTest", () => {
+  it("should return the key 'noma'", () => {
+    assert.strictEqual(result, "noma");
+  })
+})
