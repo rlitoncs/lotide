76da9ded9a1de861a90da93bc586a7e74f310446
rlitoncs <rj_liton@hotmail.com>
2024-05-09 00:05:15 -0400

    Add M&C automated testing for countLettersTest.js

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,10 +1,29 @@
 //Required modules
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const countLetters = require("../countLetters");
 
-//TEST CASES
 let result = countLetters("lighthouse in the house"); //return object
+//TEST CASES
 
-assertEqual(result["l"], 1); // Evaluate to True 1 === 1
-assertEqual(result["h"], 4); // Evalute to True 4 === 4
-assertEqual(result["e"], 3); // Evalute to True 3 === 3
+describe("#countLetters", () => {
+
+  //letter l
+  it("should return 1 occurence for letter 'l'", () => {
+    assert.strictEqual(result["l"], 1);
+  });
+
+  //letter h
+  it("should return 4 occurence for letter 'h'", () => {
+    assert.strictEqual(result["h"], 4);
+  });
+
+  //letter e
+  it("should return 3 occurence for letter 'e'", () => {
+    assert.strictEqual(result["e"], 3);
+  });
+
+  //Letter does not exist
+  it("should return undefined for non-existent letter", () => {
+    assert.strictEqual(result["z"], undefined);
+  });
+});
