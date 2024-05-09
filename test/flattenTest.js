37cbe80afb36b71ad14a7035598918cd991fca02
rlitoncs <rj_liton@hotmail.com>
2024-05-08 23:15:50 -0400

    Add M&C automated testing for flattenTest.js file

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,6 +1,12 @@
 //Require Modules
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const flatten = require("../flatten");
 
 //TEST CASE
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
+describe("#flatten", () => {
+
+  it("should return [1,2,3,4,5,6] when given [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
+  });
+
+});
